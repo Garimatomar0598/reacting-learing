25a1967f1647be6d1ca0f75b236a40a3a724f806
Garima Tomar <garimatomar55521@gmail.com>
2025-07-31 19:47:54 +0530

    learning react topics

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import Card from './Components/Card'
+
+function App() {
+
+
+
+
+  return (
+    <div>
+  <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Card.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+function Card() {
+  return (
+    <div className='w-full h-screen bg-yellow-700'> 
+    <div className='w-52 bg-red-500 absolute left-1/2 top=1/2 -transalte-x-[50%] transalte-y-[50%]'>
+    </div>   
+    </div>
+  )
+}
+
+export default Card;
