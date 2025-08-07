a2ef54df3933772e3eee7a952f4728ef25b9d5dd
Garima Tomar <garimatomar55521@gmail.com>
2025-08-07 11:05:31 +0530

    components reusebility in react

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 
 function Card() {
   return (
-    <div className='w-full h-screen bg-yellow-700'> 
-    <div className='w-52 bg-red-500 absolute left-1/2 top=1/2 -transalte-x-[50%] transalte-y-[50%]'>
-    </div>   
+    <div className='w-full h-screen bg-zinc-300'>
+      <div className='px-3 py-4 bg-zinc-400 absolute left-1/2 top-1/2 -translate-[50%] -transalte-[50%] rounded-md'></div>
+      
     </div>
   )
 }
 
-export default Card;
+export default Card
