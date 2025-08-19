bf7809390784538dbc174734543d1c98e987e1a0
Garima Tomar <garimatomar55521@gmail.com>
2025-08-19 17:09:51 +0530

    Basic level usestate understanding

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import React, { useState } from 'react'
 import Card from './Components/Card'
 
 function App() {
-  const ans = useState({name: "Garima"});
-console.log(ans[0]);
+ const [banned,setbanned] = useState(false);
+
+
 
   return (
     <>
-  
+  <div className='p-4'>
+  <h1>{banned.toString()}</h1>
+  <button onClick={()=>setbanned(!banned)} className='px-2 py-2 text-xl bg-blue-500 text-white '>change now</button>
+  </div>
     </>
   )
 }
