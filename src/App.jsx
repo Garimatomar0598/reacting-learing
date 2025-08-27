5dd233e97246117f0a37c37b23b1830bca268e75
Garima Tomar <garimatomar55521@gmail.com>
2025-08-27 14:22:43 +0530

    make small boxes and helping by props data shows into another component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
-import React, { useState } from 'react'
-
+import Card from './Components/Card'
+import React from 'react'
 
 function App() {
- const [val,setval] = useState([1,2,3,4,5,6])
   return (
-    <div className='p-8'>     
-{val.map(item=><h1>{item}</h1>)}
-<button  onClick={()=>setval(()=>{
-  return val.filter(( item,index)=>index !=val.length-1)
-})}
-
-
- className='py-2 px-5 bg-blue-600 text-xs-text-white rounded-full'>Click me</button>
- </div>
+    <div>
+      <Card text="Download Now" />
+      <Card text = "Know more " />
+    </div>
   )
-} 
+}
 
 export default App
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,40 +1,12 @@
 
 import React from 'react'
 
-function Card() {
-
-  const data = [
-{image:'https://images.unsplash.com/photo-1662947368791-8630979e964b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzR8fGFtYXpvbnxlbnwwfHwwfHx8MA%3D%3D',name:"Daily objects",description:"my initial approach consisted of writing some simple tests, such as checking.", InStock:true},
-{image:'https://plus.unsplash.com/premium_photo-1664201890376-b735362539bb?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDF8fGFtYXpvbnxlbnwwfHwwfHx8MA%3D%3D',name:"Amazon Products",description:"my initial approach consisted of writing some simple tests, such as checking.",InStock:false},
-{image:'https://images.unsplash.com/photo-1624521793559-136bfe16fc86?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGFtYXpvbnxlbnwwfHwwfHx8MA%3D%3D',name:"Amazon Deals",description:"this is amazon deals upto 50% ,enjoy your best time here", InStock:true}
-
-  ]
-
+function Card({text}) {
   return (
-    <div className='w-full h-screen flex items-center justify-center gap-10 bg-zinc-200'>
-      {data.map((elem,index)=>(
-<div key={index} className= 'w-52 bg-zinc-100  rounded-md overflow-hidden'>
-<div className='h-32 bg-zinc-300'>
-  <img src={elem.image}
-  />
-</div>
-<div  className='w-full px-3 py-4 bg-cyan-300'>
-  <h2 className='font-semibold'>{elem.name}</h2>
-  <p className='text-xs mt-3'>{elem.description}</p>
- <button
-  className={`px-4 py-1 ${elem.InStock ? 'bg-sky-700' : 'bg-red-600'} text-xs rounded text-zinc-800 mt-3`}
->{elem.InStock ? "In Stock" : "Out of Stock"}
-</button>
-
-
-</div>
-
-</div>
-
-      ))}  
-      
+    <div>
+      <button  className='py-1 px-3 mt-2 bg-blue-500 text-xs'>{text} </button>
     </div>
   )
 }
 
-export default Card;
+export default Card
