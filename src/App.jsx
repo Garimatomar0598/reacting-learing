281b85ba4e26b14ddd3828a84adda937a51fa6fc
Garima Tomar <garimatomar55521@gmail.com>
2025-08-27 13:50:40 +0530

    learn props drilling in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-
 import React, { useState } from 'react'
 
 
 function App() {
- const [banned,setbanned] = useState(false);
-
-
-
+ const [val,setval] = useState([1,2,3,4,5,6])
   return (
-    <>
-  <div className='p-4'>
-  <h1>{banned.toString()}</h1>
-  <button onClick={()=>setbanned(!banned)} className='px-2 py-2 text-xl bg-blue-500 text-white '>change now</button>
-  </div>
-    </>
-  )
-}
+    <div className='p-8'>     
+{val.map(item=><h1>{item}</h1>)}
+<button  onClick={()=>setval(()=>{
+  return val.filter(( item,index)=>index !=val.length-1)
+})}
 
-export default App;
+
+ className='py-2 px-5 bg-blue-600 text-xs-text-white rounded-full'>Click me</button>
+ </div>
+  )
+} 
+
+export default App
