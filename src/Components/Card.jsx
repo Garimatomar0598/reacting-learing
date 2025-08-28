d771f223627db611a90b7544d834182264f99dee
Garima Tomar <garimatomar55521@gmail.com>
2025-08-28 11:49:30 +0530

    i understand props drilling event handling in react

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,11 +1,17 @@
 
 import React from 'react'
 
-function Card(){
+function Card({image,name,profession}){
   return (
-    <div className='w-52 bg-zinc-500'>
-      <div className='w-full h-34 bg-sky-400'></div>
-      <div className='w-full p-3'></div>
+    <div className='w-52 bg-white p-3 overflow-hidden'>
+      <div className='w-full h-32 bg-sky-200'>
+        <img className='w-full h-full object-cover' src={image} alt=""/>
+      </div>
+      <div className='w-full p-3'>
+        <h3 className='text-xl font-extrabold'>{name}</h3>
+        <h4 className='text-xs'>{profession}</h4>
+        <button onClick={()=>alert("adding as a friend")} className='px-3 py-1 text-xs text-white bg-blue-500 mt-4'>Add Friend</button>
+      </div>
 
 
     </div>
